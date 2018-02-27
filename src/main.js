337fe6a7238b1b028bea5f0d7330e51fb226461a
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-02-26 18:44:07 -0800

    imported VueLocalStorage

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLocalStorage from 'vue-ls'
 
+let options = {
+  namespace: 'catalog__'
+};
 
-Vue.config.productionTip = false
+Vue.use(VueLocalStorage, options);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
