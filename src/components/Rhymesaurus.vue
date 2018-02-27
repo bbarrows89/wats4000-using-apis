b45d9105e1c765d4744acc111502ecb53a3e48f1
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-02-26 19:38:49 -0800

    changed fetchAPIResults

--- src/components/Rhymesaurus.vue
+++ src/components/Rhymesaurus.vue
@@ -62,14 +62,12 @@ export default {
     },
     fetchAPIResults: function() {
       this.results = null;
-      let queryID = `${this.phrase}-${this.soundsLike}-${this.startLetter}-${this.endLetter}`;
+      let queryID = `${this.phrase}-${this.rhyme}`;
       let cacheExpiry = 24 * 60 * 60 * 1000; // 24 hr cache expiry
       axios.get('https://api.datamuse.com/words', {
         params: {
           ml: this.phrase,
-          rel_rhy: this.rhyme,
-          sl: this.soundsLike,
-          sp: `${this.startLetter}*${this.endLetter}`
+          rel_rhy: this.rhyme
         }
       })
       .then( response => {
